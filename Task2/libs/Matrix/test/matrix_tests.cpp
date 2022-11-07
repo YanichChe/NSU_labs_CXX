3976caaf4bc821e9e1cc6dc55d6ee92b5bf97415
yannie <ychernovskaya@gmail.com>
2022-11-08 01:07:53 +0700

    add tests

--- Task2/libs/Matrix/test/matrix_tests.cpp
+++ Task2/libs/Matrix/test/matrix_tests.cpp
@@ -1,30 +1,28 @@
 #include "gtest/gtest.h"
 #include "../include/matrix.h"
 
-/*TEST (constructor_tests, base_file) {
-    Matrix matrix("points.txt");
+TEST(constructor_tests, base_file)
+{
+    Matrix matrix("C:/Users/Yana228/LABS_NSU/Task2/libs/GameData/points.txt");
 
-    std::array<std::array<int, PLAYERS_NUMBER>, 8> data=
-      {{
-          { 4, 4, 4 },
-          { 3, 3, 9 },
-          { 3, 9, 3 },
-          { 0, 5, 5 },
-          { 9, 3, 3 },
-          { 5, 0, 5 },
-          { 5, 5, 0 },
-          { 1, 1, 1 }
-       }};
+    std::array<std::array<int, PLAYERS_NUMBER>, 8> data =
+        {{{4, 4, 4},
+          {3, 3, 9},
+          {3, 9, 3},
+          {0, 5, 5},
+          {9, 3, 3},
+          {5, 0, 5},
+          {5, 5, 0},
+          {1, 1, 1}}};
 
-    std::array<std::array<int, PLAYERS_NUMBER>, 8> resultingMatrix;
+    std::array<std::array<int, PLAYERS_NUMBER>, 8> resultingMatrix = matrix.GetMatrix();
 
     const int row = 8, column = PLAYERS_NUMBER;
 
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
             EXPECT_EQ(data[i][j], resultingMatrix[i][j]);
-
-}*/
+}
 
 TEST(constructor_tests, not_file_found)
 {
