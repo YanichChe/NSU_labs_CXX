bc17ed4bd8cc25933743bbafb911ea921dedf831
yannie <ychernovskaya@gmail.com>
2022-11-09 02:13:34 +0700

    add Game class

--- Task2/libs/Game/include/game.h
+++ Task2/libs/Game/include/game.h
@@ -1,11 +1,12 @@
 #ifndef TASK2_GAME_H
 #define TASK2_GAME_H
-#include "../../GameData/include/gameData.h"
-#include "../../Matrix/include/matrix.h"
-#include "../../Mode/include/modeFactory.h"
+#include "gameData.h"
+#include "modeFactory.h"
 
-class Game {
-    void StartGame(GameData* data, std::string modeName);
+class Game
+{
+public:
+    void StartGame(GameData *data, std::string modeName);
 };
 
-#endif //TASK2_GAME_H
+#endif // TASK2_GAME_H
