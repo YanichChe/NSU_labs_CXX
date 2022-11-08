fea296331deafd244c9d987da3d929fb91c017a0
yannie <ychernovskaya@gmail.com>
2022-11-09 02:15:19 +0700

    update main.cpp

--- Task2/src/main.cpp
+++ Task2/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "gameData.h"
+#include "commandLineParser.h"
+#include "game.h"
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
+
+    Options options;
+    if (!SetOptionsValue(argc, argv, options))
+    {
+        return EXIT_SUCCESS;
+    }
+
+    GameData gameData(options.strategyNames,
+                      options.steps,
+                      options.configs,
+                      options.matrixFile);
+    Game game;
+    game.StartGame(&gameData, options.mode);
 
     return 0;
 }
