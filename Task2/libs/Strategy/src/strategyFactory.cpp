d18ef983b4e62b65eccad3b73a11e8facdf4fe5e
Yana Сhernovskaya <55744073+YanichChe@users.noreply.github.com>
2022-11-13 01:10:04 +0700

    change style strategyFactory

--- Task2/libs/Strategy/src/strategyFactory.cpp
+++ Task2/libs/Strategy/src/strategyFactory.cpp
@@ -2,14 +2,9 @@
 
 StrategyPointer StrategyFactory::createStrategy(std::string strategyName, std::string configs)
 {
-    if (strategyName == "alwaysCooperate")
-        return std::make_unique<AlwaysCooperate>();
-    else if (strategyName == "alwaysDefect")
-        return std::make_unique<AlwaysDefect>();
-    else if (strategyName == "random")
-        return std::make_unique<Random>();
-    else if (strategyName == "titForTat")
-        return std::make_unique<TitForTat>();
-    else if (strategyName == "softGrudger")
-        return std::make_unique<SoftGrudger>();
+    if (strategyName == "alwaysCooperate")      return std::make_unique<AlwaysCooperate>();
+    else if (strategyName == "alwaysDefect")    return std::make_unique<AlwaysDefect>();
+    else if (strategyName == "random")          return std::make_unique<Random>();
+    else if (strategyName == "titForTat")       return std::make_unique<TitForTat>();
+    else if (strategyName == "softGrudger")     return std::make_unique<SoftGrudger>();
 }
