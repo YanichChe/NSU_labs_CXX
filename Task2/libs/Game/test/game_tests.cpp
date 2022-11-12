54e0ac29d3bb996e11437dc7919ce84a9b7195c8
yannie <ychernovskaya@gmail.com>
2022-11-13 01:01:19 +0700

    add tests in Game

---
+++ Task2/libs/Game/test/game_tests.cpp
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+#include "game.h"
+
+TEST(constructor_tests, fast_mode)
+{
+    std::vector<std::string> strategiesNames = {"alwaysCooperate", "alwaysDefect", "random"};
+    GameData data(strategiesNames, 5, "meow", "C:/Users/Yana228/LABS_NSU/Task2/libs/GameData/points.txt");
+
+    ModeFactory modeFactory;
+    ModePointer fast = modeFactory.Create("fast", &data);
+
+    EXPECT_NO_THROW({ fast->Start(); });
+}
+
+TEST(constructor_tests, tournament_mode)
+{
+    std::vector<std::string> strategiesNames = {"alwaysCooperate", "alwaysDefect", "random", "titForTat", "softGrudger"};
+    GameData data(strategiesNames, 10, "meow", "C:/Users/Yana228/LABS_NSU/Task2/libs/GameData/points.txt");
+
+    ModeFactory modeFactory;
+    ModePointer tournament = modeFactory.Create("tournament", &data);
+
+    EXPECT_NO_THROW({ tournament->Start(); });
+}
+
+TEST(mode_factory_tests, wrong_mode_name)
+{
+    GameData data;
+    ModeFactory factory;
+    try
+    {
+        ModePointer mode = factory.Create("lalala", &data);
+    }
+    catch (const NotFoundMode &exception)
+    {
+        EXPECT_STREQ(exception.what(), "lalala not found mode");
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
