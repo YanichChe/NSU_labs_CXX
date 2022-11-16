cda2a978a77b845b92c51b43d7f5f45cac21d286
yannie <ychernovskaya@gmail.com>
2022-11-16 22:13:22 +0700

    change style

--- Task2/libs/Mode/src/modeFactory.cpp
+++ Task2/libs/Mode/src/modeFactory.cpp
@@ -1,15 +1,12 @@
 #include "modeFactory.h"
 
-NotFoundMode ::NotFoundMode(const std::string &mode) : std::invalid_argument(mode + " not found mode") {}
+NotFoundMode ::NotFoundMode(const std::string &mode) :
+                            std::invalid_argument(mode + " not found mode") {}
 
 ModePointer ModeFactory::Create(std::string mode, GameData *data)
 {
-    if (mode == "detailed")
-        return std::make_unique<DetailedMode>(data);
-    else if (mode == "fast")
-        return std::make_unique<FastMode>(data);
-    else if (mode == "tournament")
-        return std::make_unique<TournamentMode>(data);
-    else
-        throw NotFoundMode(mode);
+    if (mode == "detailed")         return std::make_unique<DetailedMode>(data);
+    else if (mode == "fast")        return std::make_unique<FastMode>(data);
+    else if (mode == "tournament")  return std::make_unique<TournamentMode>(data);
+    else                            throw NotFoundMode(mode);
 }
