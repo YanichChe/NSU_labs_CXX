2d5af395cff5865a6566049c10eae0f8dd874183
yannie <ychernovskaya@gmail.com>
2022-12-01 00:12:02 +0700

    add extra constants

--- Task3/libs/WAV/include/wav.h
+++ Task3/libs/WAV/include/wav.h
@@ -9,6 +9,7 @@ const uint32_t RIFF_CHUNK_ID = 0x46464952;
 const uint32_t WAVE_FORMAT = 0x45564157;
 
 const uint32_t FMT_CHUNK_ID = 0x20746d66;
+const uint16_t FMT_CHUNK_SIZE = 16; // FOR PCM
 const uint16_t AUDIO_FORMAT = 0x0001;
 const uint16_t NUMBER_CHANNELS = 1;
 const uint32_t SAMPLES_PER_SEC = 44100;
@@ -19,6 +20,7 @@ const uint16_t BLOCK_ALIGN = BITS_PER_SAMPLE * NUMBER_CHANNELS / BYTE_SIZE;
 const uint32_t BYTES_PER_SEC = BLOCK_ALIGN * SAMPLES_PER_SEC;
 
 const uint32_t DATA_CHUNK_ID = 0x61746164;
+const uint16_t TOTAL_HEADER_SIZE  = 44;
 //-----------------------------------------------------------------------------
 
 struct Header
@@ -27,16 +29,16 @@ struct Header
     uint32_t ChunkSize;
 };
 
+
 struct WAV
 {
     /* RIFF Chunk  */
-    uint32_t RIFFChunkId;
-    uint32_t RIFFChunkSize;
+    Header RIFFHeader;
     uint32_t WAVEFormat;
 
     /* fmt sub-chunk */
-    uint32_t FMTChunkId;
-    uint32_t FMTChunk1Size;
+    Header FMTHeader;
+
     uint16_t audioFormat;
     uint16_t numberChannels;
     uint32_t samplesPerSec;
@@ -45,8 +47,8 @@ struct WAV
     uint16_t bitsPerSample;
 
     /* data sub-chunk */
-    uint32_t dataChunkId;
-    uint32_t dataChunkSize;
+    Header dataHeader;
+
 };
 
 #endif // LAB1_WAV_H
